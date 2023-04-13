28ddf1e30913452b4e5a9a526b7411a58f09af0e
humi53 <j.k.humi9@gmail.com>
2023-04-13 16:40:30 +0900

    2023_04_13

--- hello_01/src/com/callor/hello/data/HelloKorea.java
+++ hello_01/src/com/callor/hello/data/HelloKorea.java
@@ -1,16 +1,10 @@
-package com.callor.hello;
+package com.callor.hello.data;
 
-/*
- * 주석, Comment
- * Source Code, Java Class File
- * 
- */
-public class HelloYour {
+public class HelloKorea {
 	public static void main(String[] args) {
 		System.out.println(30 + 40);
 		System.out.println(30 - 40);
 		System.out.println(30 * 40);
 		System.out.println(30 / 40);
-		System.out.println(30 % 40);
 	}
 }
