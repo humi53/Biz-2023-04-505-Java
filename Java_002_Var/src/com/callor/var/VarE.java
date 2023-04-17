1377288aae82b154594df13fd72c02565efe4bc3
humi53 <j.k.humi9@gmail.com>
2023-04-17 10:51:53 +0900

    변수_재활용

---
+++ Java_002_Var/src/com/callor/var/VarE.java
@@ -0,0 +1,16 @@
+package com.callor.var;
+
+public class VarE {
+	public static void main(String[] args) {
+		int num1 = 0, num2 = 0;
+		num1 = 30;
+		num2 = 40;
+		
+		int plus1=0, minus1=0, times1=0, divide1=0;
+		plus1 = num1 + num2;
+		minus1 = num1 - num2;
+		times1 = num1 * num2;
+		divide1 = num1 / num2;
+		System.out.printf("plus %d, minus %d, %% times %d, divide %d", plus1, minus1, times1, divide1);
+	}
+}
