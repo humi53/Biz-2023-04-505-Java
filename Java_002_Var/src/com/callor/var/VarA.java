c9b220a1fb6ab7d42916e9bee2dbf4d285398d19
humi53 <j.k.humi9@gmail.com>
2023-04-17 09:48:46 +0900

    var

---
+++ Java_002_Var/src/com/callor/var/VarA.java
@@ -0,0 +1,16 @@
+package com.callor.var;
+
+public class VarA {
+	public static void main(String[] args) {
+		int num1=0, num2=0;
+		
+		num1 = 30;
+		num2 = 40;
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+		
+	}
+}
